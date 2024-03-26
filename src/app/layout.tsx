59685d0356afd34a585883f02c59a757249bc756
Daniel Montillo <Danielmontillo55@gmail.com>
2024-03-26 11:24:48 -0500

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from '@components/navbar';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} w-screen h-screen flex flex-col`}>
         <Navbar />
         <section className="w-full h-full">{children}</section>
+        <Analytics/>
       </body>
     </html>
   );
